cb8b3bbffa1b94066742afcaf6227a8a35d95d7b
Paul Thomas <paulthomas2107@gmail.com>
2023-04-07 12:14:01 +0100

    More

---
+++ app/about/team/page.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function TeamPage() {
+  return (
+    <div>
+      <h1>Our Team</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique at,
+        doloremque illo veritatis magnam sunt! Error molestias iste laboriosam,
+        temporibus esse necessitatibus nam labore earum recusandae dolorum
+        exercitationem, aliquam ex!
+        </p>
+    </div>
+  );
+}
+
+export default TeamPage;
