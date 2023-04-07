707aafae0bc83cc85be561679b66ede8e4d9d612
Paul Thomas <paulthomas2107@gmail.com>
2023-04-07 12:00:03 +0100

    More

---
+++ app/about/page.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>About Travsey Media</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id sint enim
+        vero atque voluptates perspiciatis fugit eligendi ipsam consequuntur
+        veniam laborum ad quo corrupti quisquam eos delectus omnis aspernatur,
+        nihil expedita at ducimus alias, tenetur molestias! Eligendi facere,
+        harum voluptate quisquam reprehenderit minus quaerat maiores! Sit est
+        illo molestiae corporis!
+      </p>
+    </div>
+  );
+};
+
+export default AboutPage;
